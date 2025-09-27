5ed2382a7bed3354b98fbee30ea99d0d028a2716
Eduarda Rezende <soaressilvamariaeduarda639@gmail.com>
2025-09-27 12:00:37 -0300

    Cria componente footer

--- src/app/features/sobre/sobre.ts
+++ src/app/features/sobre/sobre.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavMenu } from "../../shared/components/nav-menu/nav-menu";
+import { Footer } from "../../shared/components/footer/footer";
 
 @Component({
   selector: 'app-sobre',
-  imports: [NavMenu],
+  imports: [NavMenu, Footer],
   templateUrl: './sobre.html',
   styleUrl: './sobre.scss'
 })
