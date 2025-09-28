717637710d495eb71a9b9ce835771eb6867fd1c1
Eduarda Rezende <soaressilvamariaeduarda639@gmail.com>
2025-09-28 16:08:09 -0300

    create generic button component with support for routerLink and href

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Grupos } from "../grupos/grupos";
+import { Button } from "../../shared/components/button/button";
 
 @Component({
   selector: 'app-home',
-  imports: [Grupos],
+  imports: [Button],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
---
+++ src/app/shared/components/button/button.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-button',
+  imports: [CommonModule, RouterModule],
+  templateUrl: './button.html',
+  styleUrl: './button.scss'
+})
+export class Button {
+  showIcon= true;
+  @Input() label: string = "Entrar no grupo";
+  @Input() type: "button" | "submit" = "button";
+  @Input() color: "link" | "primary" | "secundary" | "warning" = "link";
+  
+  //Link
+  @Input() routerLink?:string; 
+  @Input() href?:string;
+  @Input() target: "_self" | "_blank" = "_self"
+
+
+  @Output() click = new EventEmitter<void>();
+
+  handleClick(){
+    if(!this.routerLink && !this.href){
+      this.click.emit()
+    }
+  }
+}
