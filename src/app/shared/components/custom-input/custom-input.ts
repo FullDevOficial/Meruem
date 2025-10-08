6a718d086164529e051435eea848db0b73ef247b
Eduarda Rezende <soaressilvamariaeduarda639@gmail.com>
2025-10-07 23:34:47 -0300

    create generic InputComponent

---
+++ src/app/shared/components/custom-input/custom-input.ts
@@ -0,0 +1,69 @@
+import { Component, Input } from '@angular/core';
+import { MaterialModule } from "../../material.module";
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-custom-input',
+  imports: [MaterialModule, ReactiveFormsModule, CommonModule],
+  styleUrl: './custom-input.scss',
+  template: `
+    <mat-form-field>
+      <mat-label> {{ label }} </mat-label>
+
+      @if(type === 'textarea') { 
+        <textarea matInput [formControl]="control" placeholder="placeholder"></textarea> 
+      }
+      @if(type !== 'textarea') {
+        <input matInput [formControl]="control" [placeholder]="placeholder" [type]="type" [attr.minlength]="minlength" [attr.maxlength]="maxlength"/> 
+      }
+      @if(required === true) {<mat-hint> O campo {{ label}}  é obrigatório</mat-hint> }
+      
+      <!-- Utilizado para informações adicionais sobre o campo oferecendo dicas para o usuário -->
+      @if(hint) { <mat-hint>{{ hint }}</mat-hint> }
+      <!-- Utilizado para mensagens de erro -->
+      <mat-error> {{ errorMessage }}  </mat-error>
+
+    </mat-form-field>
+  `
+})
+export class InputComponent {
+
+  @Input() control!: FormControl;
+  @Input() label!: string;
+  @Input() placeholder = '';
+  @Input() type: "text" | "email" | "tel" | "number" | "textarea" = "text";
+  @Input() hint?: string;
+  @Input() minlength?: number;
+  @Input() maxlength?: number;
+
+  @Input() required = false;
+  @Input() disabled = false;
+
+  get errorMessage(): string | null {
+    if (!this.control || !this.control.errors || !this.control.touched) return null;
+
+    if (this.control.errors['required']) {
+      return ` O ${this.label} é obrigatório`;
+    }
+
+    if (this.control.errors['email']) {
+      return ` O ${this.label} é inválido`;
+    }
+
+    if (this.control.errors['minlength']) {
+      const min = this.control.errors['minlength'].requiredLength;
+      return `O ${this.label} deve ter no mínimo ${min} caracteres`;
+    }
+
+    if (this.control.errors['maxlength']) {
+      const max = this.control.errors['maxlength'].requiredLength;
+      return `O ${this.label} deve ter no máximo ${max} caracteres`;
+    }
+
+    return null
+  }
+
+
+}
